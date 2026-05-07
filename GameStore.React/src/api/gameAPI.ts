f58ed99a2d279456d04dd7493a1ae7c81b730e1e
LAPTOP-140RNLEU\Hansu kumar <hanshukumar8459@gmail.com>
2026-05-07 22:08:40 +0530

    Updated API URL for production

--- GameStore.React/src/api/gameAPI.ts
+++ GameStore.React/src/api/gameAPI.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
 import type { Game } from "../types/Game";
 
-const API = "/api"; // because of proxy
+const API = "https://gamestore-7wju.onrender.com";
 
 export const getGames = () => axios.get(`${API}/games`);
-export const getGame = (id: number) => axios.get(`${API}/games/${id}`);
-export const createGame = (game: Game) => axios.post(`${API}/games`, game);
+
+export const getGame = (id: number) =>
+  axios.get(`${API}/games/${id}`);
+
+export const createGame = (game: Game) =>
+  axios.post(`${API}/games`, game);
+
 export const updateGame = (id: number, game: Game) =>
   axios.put(`${API}/games/${id}`, game);
+
 export const deleteGame = (id: number) =>
   axios.delete(`${API}/games/${id}`);
 
-export const getGenres = () => axios.get(`${API}/genres`);
+export const getGenres = () =>
+  axios.get(`${API}/genres`);
